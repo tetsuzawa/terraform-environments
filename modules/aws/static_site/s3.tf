0cf893b606e5c3ee091b5d8ad590698971ea281d
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-02-15 23:01:37 +0900

    Remove description of workspace

--- modules/aws/static_site/s3.tf
+++ modules/aws/static_site/s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "static_site" {
-  bucket        = "${terraform.workspace}-${var.bucket_name}"
+  bucket        = var.bucket_name
   force_destroy = true
 }
 
 resource "aws_s3_bucket" "static_site_access_logs" {
-  bucket        = "${terraform.workspace}-${var.bucket_name}-logs"
+  bucket        = "${var.bucket_name}-logs"
   force_destroy = true
 }
 
